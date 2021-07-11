767bc262eb264abdfa3eda31289ac6b6f3c3d70f
Carlos Munoz Diaz <carlos10md@gmail.com>
2021-07-11 13:27:32 +0200

    Desarrollo del tooltip: posicionamiento

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,19 +1,47 @@
-let tooltip = d3.select('#chartTooltip');
+let tooltip = d3.select('.chart__tooltip');
+let chart = d3.select('.chart__viz');
 
-function getInTooltip() {
-    tooltip
-        .transition()
-        .duration(500)
-        .style('opacity', 1);
+chart.on('mouseenter', function() {
+    setTooltipText();
+});
+
+chart.on('mousemove', function() {    
+    positionTooltip(this);
+    getInTooltip();
+});
+
+chart.on('mouseout', function() {
+    getOutTooltip();
+});
+
+/*
+* FUNCIONES TOOLTIP
+*/
+
+function setTooltipText() { //Mouseenter
+    tooltip.html('Pruebaaaaa doble prueba y hasta triple prueba');
 }
 
-function getOutTooltip() {
-    tooltip
-        .transition()
-        .duration(500)
-        .style('opacity', 0);
+function getInTooltip() { //Mouseover
+    tooltip.style('opacity', 1);
 }
 
-function positionTooltip() {
+function getOutTooltip() { //Mouseout
+    tooltip.transition().duration(500).style('opacity', 0);
+}
+
+function positionTooltip(div) {
+    let coordinates = d3.mouse(div);
+
+    let x = coordinates[0];
+    let y = coordinates[1];
+    let tooltipWidth = parseInt(tooltip.style('width'));
+    let tooltipHeight = parseInt(tooltip.style('height'));
     
+    //Posición
+    let left = div.getBoundingClientRect().width / 2 > x ? 'right' : 'left';
+    let horizontalPos = left == 'left' ? -tooltipWidth - 30 : 30;
+
+    tooltip.style('top', y - (tooltipHeight + 15) + 'px');
+    tooltip.style('left', x + horizontalPos + 'px');
 }
