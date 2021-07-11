4814d01ad6eeccb73be8ca65caf89d4326c25a0b
Carlos Munoz Diaz <carlos10md@gmail.com>
2021-07-11 18:41:22 +0200

    Desarrollo del primer gráfico (sin animación)

--- docs/js/main.js
+++ docs/js/main.js
@@ -22,15 +22,16 @@ function getFirstChart() {
         //Disposición del eje X
         let x = d3.scaleBand()
             .domain(data.map(function(d) { return d.Fecha }))
-            .range([0, width]);
+            .range([0, width])
+            .paddingInner(0.15);
 
         //Estilos para eje X
         let xAxis = function(g){
-            g.call(d3.axisBottom(x))
+            g.call(d3.axisBottom(x).tickValues(x.domain().filter(function(d,i){ return !(i%4)})))
             g.call(function(g){
                 g.selectAll('.tick line')
                     .attr('y1', '0%')
-                    .attr('y2', '-100%')
+                    .attr('y2', `-${height}`)
             })
             g.call(function(g){g.select('.domain').remove()});
         }
@@ -43,7 +44,8 @@ function getFirstChart() {
         //Disposición del eje Y
         let y = d3.scaleLinear()
             .domain([-10,10])
-            .range([height,0]);
+            .range([height,0])
+            .nice();
     
         let yAxis = function(svg){
             svg.call(d3.axisLeft(y))
@@ -54,14 +56,188 @@ function getFirstChart() {
                             return 'line-special';
                         }
                     })
-                    .attr("x1", '0%')
-                    .attr("x2", '100%')
+                    .attr("x1", `${x.bandwidth() / 2}`)
+                    .attr("x2", `${width - x.bandwidth() / 2}`)
             })
             svg.call(function(g){g.select('.domain').remove()})
         }        
         
         chart.append("g")
             .call(yAxis);
+
+        //Inicialización de líneas
+        let lineAE = d3.line()
+            .x(function(d) { return x(d.Fecha) + x.bandwidth() / 2; })
+            .y(function(d) { return y(d.AE); });
+
+        let lineLAC = d3.line()
+            .x(function(d) { return x(d.Fecha) + x.bandwidth() / 2; })
+            .y(function(d) { return y(d.LAC); });
+
+        let lineEE = d3.line()
+            .x(function(d) { return x(d.Fecha) + x.bandwidth() / 2; })
+            .y(function(d) { return y(d.EE); });
+
+        //Visualización
+        chart.append("path")
+            .data([data])
+            .attr("class", "line line-ae")
+            .attr("fill", "none")
+            .attr("stroke", '#99E6FC')
+            .attr("stroke-width", '1.5px')
+            .attr("d", lineAE);
+
+        chart.append("path")
+            .data([data])
+            .attr("class", "line line-lac")
+            .attr("fill", "none")
+            .attr("stroke", '#2347E3')
+            .attr("stroke-width", '1.5px')
+            .attr("d", lineLAC);
+
+        chart.append("path")
+            .data([data])
+            .attr("class", "line line-ee")
+            .attr("fill", "none")
+            .attr("stroke", '#081C29')
+            .attr("stroke-width", '1.5px')
+            .attr("d", lineEE);
+
+        //Pintado de círculos ocultos para tooltip
+        chart.selectAll('.circles')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('class', 'circle-ae')
+            .attr("r", 5)
+            .attr("cx", function(d) { return x(d.Fecha) + x.bandwidth() / 2; })
+            .attr("cy", function(d) { return y(d.AE); })
+            .style("fill", '#000')
+            .style('opacity', '0')
+            .on('mouseenter', function(d, i, e) {
+                let html = `<p class="chart__tooltip--title">Año ${d.Fecha}</p>
+                            <p class="chart__tooltip--text">Economías avanzadas: ${d.AE}%</p>`;
+
+                tooltip.html(html);
+            })
+            .on('mousemove', function(d, i, e) {
+                //Posibilidad visualización línea diferente
+                let lines = document.getElementsByClassName('line');
+                let css = e[i].getAttribute('class').split('-')[1];
+
+                for(let i = 0; i < lines.length; i++) {
+                    lines[i].style.opacity = '0.4';
+                    if(lines[i].getAttribute('class').indexOf(`line-${css}`) != -1) {
+                        lines[i].style.opacity = '1';
+                        lines[i].style.strokeWidth = '2.5px';
+                    }
+                }
+
+                //Tooltip
+                positionTooltip(tooltip, e[i], chartBlock);
+                getInTooltip(tooltip);               
+            })
+            .on('mouseout', function(d, i, e) {
+                //Quitamos los estilos de la línea
+                let lines = document.getElementsByClassName('line');
+                for(let i = 0; i < lines.length; i++) {
+                    lines[i].style.opacity = '1';
+                    lines[i].style.strokeWidth = '1.5px';
+                }
+
+                //Quitamos el tooltip
+                getOutTooltip(tooltip);                
+            });
+
+        chart.selectAll('.circles')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('class', 'circle-lac')
+            .attr("r", 5)
+            .attr("cx", function(d) { return x(d.Fecha) + x.bandwidth() / 2; })
+            .attr("cy", function(d) { return y(d.LAC); })
+            .style("fill", '#000')
+            .style('opacity', '0')
+            .on('mouseenter', function(d, i, e) {
+                let html = `<p class="chart__tooltip--title">Año ${d.Fecha}</p>
+                            <p class="chart__tooltip--text">Economías avanzadas: ${d.LAC}%</p>`;
+
+                tooltip.html(html);
+            })
+            .on('mousemove', function(d, i, e) {
+                //Posibilidad visualización línea diferente
+                let lines = document.getElementsByClassName('line');
+                let css = e[i].getAttribute('class').split('-')[1];
+
+                for(let i = 0; i < lines.length; i++) {
+                    lines[i].style.opacity = '0.4';
+                    if(lines[i].getAttribute('class').indexOf(`line-${css}`) != -1) {
+                        lines[i].style.opacity = '1';
+                        lines[i].style.strokeWidth = '2.5px';
+                    }
+                }
+
+                //Tooltip
+                positionTooltip(tooltip, e[i], chartBlock);
+                getInTooltip(tooltip);               
+            })
+            .on('mouseout', function(d, i, e) {
+                //Quitamos los estilos de la línea
+                let lines = document.getElementsByClassName('line');
+                for(let i = 0; i < lines.length; i++) {
+                    lines[i].style.opacity = '1';
+                    lines[i].style.strokeWidth = '1.5px';
+                }
+
+                //Quitamos el tooltip
+                getOutTooltip(tooltip);                
+            });
+
+        chart.selectAll('.circles')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('class', 'circle-ee')
+            .attr("r", 5)
+            .attr("cx", function(d) { return x(d.Fecha) + x.bandwidth() / 2; })
+            .attr("cy", function(d) { return y(d.EE); })
+            .style("fill", '#000')
+            .style('opacity', '0')
+            .on('mouseenter', function(d, i, e) {
+                let html = `<p class="chart__tooltip--title">Año ${d.Fecha}</p>
+                            <p class="chart__tooltip--text">Economías avanzadas: ${d.EE}%</p>`;
+
+                tooltip.html(html);
+            })
+            .on('mousemove', function(d, i, e) {
+                //Posibilidad visualización línea diferente
+                let lines = document.getElementsByClassName('line');
+                let css = e[i].getAttribute('class').split('-')[1];
+
+                for(let i = 0; i < lines.length; i++) {
+                    lines[i].style.opacity = '0.4';
+                    if(lines[i].getAttribute('class').indexOf(`line-${css}`) != -1) {
+                        lines[i].style.opacity = '1';
+                        lines[i].style.strokeWidth = '2.5px';
+                    }
+                }
+
+                //Tooltip
+                positionTooltip(tooltip, e[i], chartBlock);
+                getInTooltip(tooltip);               
+            })
+            .on('mouseout', function(d, i, e) {
+                //Quitamos los estilos de la línea
+                let lines = document.getElementsByClassName('line');
+                for(let i = 0; i < lines.length; i++) {
+                    lines[i].style.opacity = '1';
+                    lines[i].style.strokeWidth = '1.5px';
+                }
+
+                //Quitamos el tooltip
+                getOutTooltip(tooltip);                
+            });
     });
 }
 
@@ -69,7 +245,7 @@ getFirstChart();
 
 /* Visualization helpers */
 
-/* Iniciaulización del gráfico */
+/* Inicialización del gráfico */
 function setChart(chartBlock) {
     let margin = {top: 5, right: 5, bottom: 25, left: 30},
     width = parseInt(chartBlock.style('width')) - margin.left - margin.right,
--- docs/js/tooltip.js
+++ docs/js/tooltip.js
@@ -1,11 +1,6 @@
 /*
 * FUNCIONES TOOLTIP
 */
-
-function setTooltipText(tooltip) { //Mouseenter
-    tooltip.html('Pruebaaaaa doble prueba y hasta triple prueba');
-}
-
 function getInTooltip(tooltip) { //Mouseover
     tooltip.style('opacity', 1);
 }
@@ -14,18 +9,19 @@ function getOutTooltip(tooltip) { //Mouseout
     tooltip.transition().duration(500).style('opacity', 0);
 }
 
-function positionTooltip(tooltip, div) {
-    let coordinates = d3.mouse(div);
+function positionTooltip(tooltip, el, div) {
+    let coordinates = d3.mouse(el);
 
     let x = coordinates[0];
     let y = coordinates[1];
+    
     let tooltipWidth = parseInt(tooltip.style('width'));
     let tooltipHeight = parseInt(tooltip.style('height'));
     
     //Posición
-    let left = div.getBoundingClientRect().width / 2 > x ? 'right' : 'left';
-    let horizontalPos = left == 'left' ? -tooltipWidth - 30 : 30;
+    let left = parseInt(div.style('width')) / 2 > x ? 'right' : 'left';
+    let horizontalPos = left == 'left' ? -tooltipWidth : 45;
 
-    tooltip.style('top', y - (tooltipHeight + 15) + 'px');
+    tooltip.style('top', y - (tooltipHeight + 20) + 'px');
     tooltip.style('left', x + horizontalPos + 'px');
 }
