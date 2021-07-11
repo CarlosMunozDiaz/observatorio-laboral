b8c5bd29341c517133e8a41a1cf02d094c7cc09f
Carlos Munoz Diaz <carlos10md@gmail.com>
2021-07-11 11:22:55 +0200

    Desarrollo casos de uso de elementos superiores

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,19 @@
-console.log("Entra");
+let tooltip = d3.select('#chartTooltip');
+
+function getInTooltip() {
+    tooltip
+        .transition()
+        .duration(500)
+        .style('opacity', 1);
+}
+
+function getOutTooltip() {
+    tooltip
+        .transition()
+        .duration(500)
+        .style('opacity', 0);
+}
+
+function positionTooltip() {
+    
+}
