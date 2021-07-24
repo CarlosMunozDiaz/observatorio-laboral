fc9f28cf2851676222d6bdddb866c3f058b86c3b
Carlos Munoz Diaz <carlos10md@gmail.com>
2021-07-24 21:38:41 +0200

    Desarrollo de gráfico 18

--- docs/js/main.js
+++ docs/js/main.js
@@ -3444,9 +3444,293 @@ function getEighteenthChart() {
 
     //Lectura de datos
     let file = './data/chart-eighteen.csv';
-    d3.csv(file, function(error, data) {
+    d3.csv(file, function(d) {
+        return {
+            Fecha: d.Fecha,
+            Promedio: +d['Promedio'].replace(/,/g, '.'),            
+            'Software_y_Servicios_IT': +d['Software y Servicios IT'].replace(/,/g, '.'),
+            'Sin_fines_de_lucro': +d['Sin fines de lucro'].replace(/,/g, '.'),
+            'Finanzas': +d['Finanzas'].replace(/,/g, '.'),
+            'Comercio_al_menudeo': +d['Comercio al menudeo'].replace(/,/g, '.'),            
+            'Salud': +d['Salud'].replace(/,/g, '.'),
+            'Transporte_y_Logística': +d['Transporte y Logística'].replace(/,/g, '.'),
+            'Medios_y_comunicación': +d['Medios y comunicación'].replace(/,/g, '.'),
+            'Bienestar_y_salud': +d['Bienestar y salud'].replace(/,/g, '.'),
+            'Bienes_raíces': +d['Bienes raíces'].replace(/,/g, '.'),
+            'Servicios_corporativos': +d['Servicios corporativos'].replace(/,/g, '.'),
+            'Agricultura': +d['Agricultura'].replace(/,/g, '.'),
+            'Construcción': +d['Construcción'].replace(/,/g, '.'),
+            'Bienes_de_consumo': +d['Bienes de consumo'].replace(/,/g, '.'),
+            'Hardware_y_redes': +d['Hardware y redes'].replace(/,/g, '.'),
+            'Legal': +d['Legal'].replace(/,/g, '.'),
+            'Manufactura': +d['Manufactura'].replace(/,/g, '.'),
+            'Energía_y_minas': +d['Energía y minas'].replace(/,/g, '.'),
+            'Entretenimiento': +d['Entretenimiento'].replace(/,/g, '.'),
+            'Diseño': +d['Diseño'].replace(/,/g, '.'),
+            'Administración_pública': +d['Administración pública'].replace(/,/g, '.'),
+            'Educación': +d['Educación'].replace(/,/g, '.'),
+            'Recreación_y_viajes': +d['Recreación y viajes'].replace(/,/g, '.')
+        }
+    }, function(error, data) {
         if (error) throw error;
-        console.log(data);
+        
+        //Creación del elemento SVG en el contenedor
+        let margin = {top: 5, right: 5, bottom: 25, left: 35};
+        let {width, height, chart} = setChart(chartBlock, margin);
+
+        //Disposición del eje X
+        let x = d3.scaleBand()
+            .domain(data.map(function(d) { return d.Fecha }))
+            .range([0, width])
+            .paddingInner(0.25);
+
+        //Estilos para eje X
+        let xAxis = function(g){
+            g.call(d3.axisBottom(x).tickValues(x.domain().filter(function(d,i){ return !(i%3)})))
+            g.call(function(g){
+                g.selectAll('.tick line')
+                    .attr('y1', '0%')
+                    .attr('y2', `-${height}`)
+            })
+            g.call(function(g){g.select('.domain').remove()});
+        }
+        
+        //Inicialización eje X
+        chart.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        //Disposición del eje Y
+        let y = d3.scaleLinear()
+            .domain([0,4])
+            .range([height,0])
+            .nice();
+    
+        let yAxis = function(svg){
+            svg.call(d3.axisLeft(y).ticks(4).tickFormat(function(d) { return d.toString().replace(/\./g, ','); }))
+            svg.call(function(g){
+                g.selectAll('.tick line')
+                    .attr('class', function(d,i) {
+                        if (d == 0) {
+                            return 'line-special';
+                        }
+                    })
+                    .attr("x1", `${x.bandwidth() / 2}`)
+                    .attr("x2", `${width - x.bandwidth() / 2}`)
+            })
+            svg.call(function(g){g.select('.domain').remove()})
+        }
+        
+        chart.append("g")
+            .call(yAxis);
+
+        let lines = [];
+        let columns = data.columns.slice(1);
+        for(let i = 0; i < columns.length; i++) {
+            let aux = {lineName: `${columns[i].replace(/\s/g, '_')}`, xAxis: 'Fecha', cssName: `${columns[i].replace(/\s/g, '_')}`};
+            lines.push(aux);
+        }
+
+        //Ordenar con Promedio primero
+        let promedio = lines.slice(12,13);
+        let aux = [...lines];
+        aux.splice(12, 1);
+        let newLines = [...promedio, ...aux];
+
+        let currentIndustry = 'null';
+
+        function initChart(lines) {
+            for(let i = 0; i < lines.length; i++) {
+                let line = d3.line()
+                    .x(function(d) { return x(d[lines[i].xAxis]) + x.bandwidth() / 2; })
+                    .y(function(d) { return y(d[lines[i].lineName]); });
+
+                if(lines[i].lineName == 'Promedio') {
+                    
+                    //Línea real
+                    let path = chart.append("path")
+                        .data([data])
+                        .attr("class", `line-${lines[i].cssName}`)
+                        .attr("fill", "none")
+                        .attr("stroke", `#99E6FC`)
+                        .attr("stroke-width", "1.5px")
+                        .attr("d", line)
+                        .on('touchstart touchmove mousemove mouseover', function(d,i,e) {
+                            //Posibilidad visualización línea diferente            
+                            let currentLine = e[i];
+            
+                            currentLine.style.opacity = '1';
+                            currentLine.style.strokeWidth = '3.5px';
+                        })
+                        .on('touchend mouseout', function(d,i,e) {
+                            //Quitamos los estilos de la línea
+                            let currentLine = e[i];
+                            currentLine.style.strokeWidth = '1.5px';
+                        });
+        
+                    let length = path.node().getTotalLength();
+            
+                    path.attr("stroke-dasharray", length + " " + length)
+                        .attr("stroke-dashoffset", length)
+                        .transition()
+                        .ease(d3.easeLinear)
+                        .attr("stroke-dashoffset", 0)
+                        .duration(4500);
+        
+                    chart.selectAll('.circles')
+                        .data(data)
+                        .enter()
+                        .append('circle')
+                        .attr('class', `circle-${lines[i].cssName}`)
+                        .attr("r", 5)
+                        .attr("cx", function(d) { return x(d[lines[i].xAxis]) + x.bandwidth() / 2; })
+                        .attr("cy", function(d) { return y(d[lines[i].lineName]); })
+                        .style("fill", '#000')
+                        .style('opacity', '0')
+                        .on('touchstart touchmove mousemove mouseover', function(d, i, e) {
+                            let css = e[i].getAttribute('class').split('-')[1];
+            
+                            //Texto
+                            let html = `<p class="chart__tooltip--title">${d.Fecha}</p>
+                                <p class="chart__tooltip--text">${css}: ${numberWithCommas(d[css])}</p>`;
+            
+                            tooltip.html(html);
+            
+                            //Posibilidad visualización línea diferente
+                            let currentLine = chartBlock.select(`.line-${css}`);
+                            currentLine.style('stroke-width','3.5px');
+            
+                            //Tooltip
+                            positionTooltip(window.event, tooltip);
+                            getInTooltip(tooltip);               
+                        })
+                        .on('touchend mouseout', function(d, i, e) {
+                            //Quitamos los estilos de la línea
+                            let css = e[i].getAttribute('class').split('-')[1];
+                            let currentLine = chartBlock.select(`.line-${css}`);
+                            currentLine.style('stroke-width','1.5px');
+            
+                            //Quitamos el tooltip
+                            getOutTooltip(tooltip);                
+                        });
+
+                } else {
+
+                    let path = chart.append("path")
+                    .data([data])
+                    .attr("class", `line line-${lines[i].cssName}`)
+                    .attr("fill", "none")
+                    .attr("stroke", `#081C29`)
+                    .attr("stroke-width", '0px')
+                    .attr("d", line)
+                    .on('touchstart touchmove mousemove mouseover', function(d,i,e) {
+                        //Posibilidad visualización línea diferente            
+                        let currentLine = e[i];
+        
+                        currentLine.style.opacity = '1';
+                        currentLine.style.strokeWidth = '3.5px';
+                    })
+                    .on('touchend mouseout', function(d,i,e) {
+                        //Quitamos los estilos de la línea
+                        let currentLine = e[i];
+                        currentLine.style.strokeWidth = '1.5px';
+                    });
+        
+                let length = path.node().getTotalLength();
+        
+                path.attr("stroke-dasharray", length + " " + length)
+                    .attr("stroke-dashoffset", length)
+                    .transition()
+                    .ease(d3.easeLinear)
+                    .attr("stroke-dashoffset", 0)
+                    .duration(4500)
+        
+                chart.selectAll('.circles')
+                    .data(data)
+                    .enter()
+                    .append('circle')
+                    .attr('class', `circle-${lines[i].cssName}`)
+                    .attr("r", 5)
+                    .attr("cx", function(d) { return x(d[lines[i].xAxis]) + x.bandwidth() / 2; })
+                    .attr("cy", function(d) { return y(d[lines[i].lineName]); })
+                    .style("fill", 'none')
+                    .style('opacity', '0')
+                    .on('touchstart touchmove mousemove mouseover', function(d, i, e) {
+                        let css = e[i].getAttribute('class').split('-')[1];
+        
+                        //Texto
+                        let html = `<p class="chart__tooltip--title">${d.Fecha}</p>
+                                    <p class="chart__tooltip--text">${css}: ${numberWithCommas(d[css])}</p>`;
+        
+                        tooltip.html(html);
+        
+                        //Posibilidad visualización línea diferente
+                        let currentLine = chartBlock.select(`.line-${css}`);
+                        currentLine.style('stroke-width','3.5px');
+
+                        //Tooltip
+                        positionTooltip(window.event, tooltip);
+                        getInTooltip(tooltip);               
+                    })
+                    .on('touchend mouseout', function(d, i, e) {
+                        //Quitamos los estilos de la línea
+                        let css = e[i].getAttribute('class').split('-')[1];
+        
+                        //Posibilidad visualización línea diferente
+                        let currentLine = chartBlock.select(`.line-${css}`);
+                        currentLine.style('stroke-width','1.5px');
+
+                        //Quitamos el tooltip
+                        getOutTooltip(tooltip);                
+                    });
+
+                }               
+            }
+        }
+
+        window.addEventListener('scroll', function() {
+            if (!chartBlock.node().classList.contains('visible')){
+                if(isElementInViewport(chartBlock.node())){
+                    chartBlock.node().classList.add('visible');
+                    initChart(newLines);
+                }                
+            }
+        });
+
+        //Se actualiza con el país seleccionado
+        function updateChart(currentIndustry, industry) {
+            //Primero quitamos la línea del país anterior >> Lógica para opacidad de línea y uso de círculos
+
+            if(currentIndustry != 'null') {
+                let formerLine = chartBlock.select(`.line-${currentIndustry}`);
+                let formerCircles = chartBlock.selectAll(`.circle-${currentIndustry}`);
+
+                formerLine.style('stroke-width','0px');
+                formerCircles.each(function() {
+                    this.style.fill = 'none';
+                });
+            }            
+
+            //Tras ello, damos nuevos estilos al país seleccionado >> Si es null, sólo mostramos 'Promedio'
+            if(industry != 'null') {
+                let line = chartBlock.select(`.line-${industry}`);
+                console.log(line);
+                let circles = chartBlock.selectAll(`.circle-${industry}`);
+
+                line.style('stroke-width','1.5px');
+                circles.each(function() {
+                    this.style.fill = '#000';
+                });
+            }
+        }
+
+        document.getElementById('empleoEighteen').addEventListener('change', function(e) {
+            let auxIndustry = currentIndustry;
+            currentIndustry = e.target.value;
+            console.log(currentIndustry, auxIndustry);
+            updateChart(auxIndustry, currentIndustry);            
+        });
+
     });
 }
 
