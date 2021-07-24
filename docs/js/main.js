85db26ffd07f445b153aab80eae7bb4b4760a2b4
Carlos Munoz Diaz <carlos10md@gmail.com>
2021-07-24 07:31:49 +0200

    Nuevo gráfico 1 + Cambio en switch

--- docs/js/main.js
+++ docs/js/main.js
@@ -9,6 +9,134 @@ function getFirstChart() {
     d3.csv(file, function(d) {
         return {
             Fecha: d.Year,
+            'América Latina y Caribe': +d['LAC'].replace(/,/g, '.')
+        }
+    }, function(error, data) {
+        if (error) throw error;
+        
+        //Creación del elemento SVG en el contenedor
+        let margin = {top: 5, right: 5, bottom: 25, left: 35};
+        let {width, height, chart} = setChart(chartBlock, margin);
+
+        //Disposición del eje X
+        let x = d3.scaleBand()
+            .domain(data.map(function(d) { return d.Fecha }))
+            .range([0, width])
+            .paddingInner(0.25);
+
+        //Estilos para eje X
+        let xAxis = function(g){
+            g.call(d3.axisBottom(x).tickValues(x.domain().filter(function(d,i){ return !(i%4)})))
+            g.call(function(g){
+                g.selectAll('.tick line')
+                    .attr('y1', '0%')
+                    .attr('y2', `-${height}`)
+            })
+            g.call(function(g){g.select('.domain').remove()});
+        }
+        
+        //Inicialización eje X
+        chart.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        //Disposición del eje Y
+        let y = d3.scaleLinear()
+            .domain([-10,10])
+            .range([height,0])
+            .nice();
+    
+        let yAxis = function(svg){
+            svg.call(d3.axisLeft(y).tickFormat(function(d) { return d + '%'; }))
+            svg.call(function(g){
+                g.selectAll('.tick line')
+                    .attr('class', function(d,i) {
+                        if (d == 0) {
+                            return 'line-special';
+                        }
+                    })
+                    .attr("x1", `${x.bandwidth() / 2}`)
+                    .attr("x2", `${width - x.bandwidth() / 2}`)
+            })
+            svg.call(function(g){g.select('.domain').remove()})
+        }        
+        
+        chart.append("g")
+            .call(yAxis);
+
+        //Inicialización de barras
+        function initChart() {
+            chart.selectAll(".bar")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr('class', function(d, i) { return `bar bar-${i}`; })
+                .style('fill', '#081C29')
+                .attr('x', function(d) { return x(d.Fecha) + x.bandwidth() / 2; })
+                .attr('width', x.bandwidth() / 2)
+                .attr("y", function(d) { return y(0); })
+                .on('touchstart touchmove mouseover mousemove', function(d, i, e) {
+                    let css = e[i].getAttribute('class').split('-')[1];
+
+                    console.log(d);
+                    
+                    //Texto
+                    let html = `<p class="chart__tooltip--title">${d.Fecha}</p>
+                                <p class="chart__tooltip--text">${numberWithCommas(d['América Latina y Caribe'])}%</p>`;
+                
+                    tooltip.html(html);
+
+                    //Posibilidad visualización línea diferente
+                    let bars = chartBlock.selectAll('.bar');                    
+                
+                    bars.each(function() {
+                        this.style.opacity = '0.4';
+                        let split = this.getAttribute('class').split(" ")[1];
+                        if(split == `bar-${css}`) {
+                            this.style.opacity = '1';
+                        }
+                    });
+                
+                    //Tooltip
+                    positionTooltip(window.event, tooltip);
+                    getInTooltip(tooltip);
+                })
+                .on('touchend mouseout', function(d, i, e) {
+                    //Quitamos los estilos de la línea
+                    let bars = chartBlock.selectAll('.bar');
+                    bars.each(function() {
+                        this.style.opacity = '1';
+                    });
+                
+                    //Quitamos el tooltip
+                    getOutTooltip(tooltip); 
+                })
+                .transition()
+                .duration(3000)
+                .attr("y", function(d) { return y(Math.max(0, d['América Latina y Caribe'])); })     
+                .attr('height', d => Math.abs(y(d['América Latina y Caribe']) - y(0)));
+        }
+
+        window.addEventListener('scroll', function() {
+            if (!chartBlock.node().classList.contains('visible')){
+                if(isElementInViewport(chartBlock.node())){
+                    chartBlock.node().classList.add('visible');
+                    initChart();
+                }                
+            }
+        });     
+    });
+}
+
+function getFirstBisChart() {
+    //Bloque de la visualización
+    let chartBlock = d3.select('#chart-one_bis');
+
+    //Lectura de datos
+    let file = './data/chart-one.csv';
+    d3.csv(file, function(d) {
+        return {
+            Fecha: d.Year,
             'Economías Avanzadas': +d['Advanced Economies'].replace(/,/g, '.'),
             'América Latina y Caribe': +d['LAC'].replace(/,/g, '.'),
             'Economías Emergentes': +d['Emerging Economies'].replace(/,/g, '.')
@@ -2938,6 +3066,7 @@ function get4_6Chart() {
 }
 
 getFirstChart();
+getFirstBisChart();
 getSecondChart();
 getSecondBisChart();
 getThirdChart();
